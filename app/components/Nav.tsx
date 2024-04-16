2bebed42be4e1950335caf95301a3510ffe69f06
Matilde Park <matilde@park.net>
2024-04-16 16:53:29 -0400

    app: scaffold 2

---
+++ app/components/Nav.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Fragment } from "react";
+import cn from "classnames";
+import { useIntersectionObserver } from "@uidotdev/usehooks";
+
+export default function NavWrapper({ children }: {
+    children: React.ReactNode;
+}) {
+  const [refFooter, entryFooter] = useIntersectionObserver({
+    threshold: 0.1,
+    root: null,
+    rootMargin: "0px",
+  });
+  const pathname = usePathname;
+  return (
+    <Fragment key="key">
+      <nav
+        className={cn(
+          "w-full bg-deep-slate border-b border-gray-600 pb-2 lg:pb-0 lg:border-none lg:bg-transparent lg:mix-blend-difference shrink-0 z-30 fixed top-0 flex flex-col lg:flex-row lg:justify-between lg:items-center screen-fadein",
+          {
+            hidden: entryFooter?.isIntersecting,
+          },
+        )}
+      >
+        <Link href="/">
+          <div className="p-2 lg:p-8 flex text-white items-center space-x-2 text-xl lg:text-4xl font-medium tracking-tighter select-none shrink-0 transition-all">
+            <p className="hover:text-terminal-magenta font-sans font-medium">
+              Nolita
+            </p>
+          </div>
+        </Link>
+        <div className="flex items-center uppercase px-2 lg:p-8 space-x-8 min-w-0 min-h-0 overflow-x-auto overflow-y-visible no-scrollbar">
+          <Link href="/blog">
+            <li
+              className={cn("font-mono px-2 py-1 list-none border hover:text-terminal-magenta hover:bg-transparent border-terminal-magenta bg-terminal-magenta text-black")}
+            >
+              GET STARTED
+            </li>
+          </Link>
+          <Link href="/blog">
+            <li
+              className={cn("font-mono list-none hover:text-terminal-magenta", {
+                "text-terminal-magenta": ("/blog" in pathname),
+                "text-white": !("/blog" in pathname),
+              })}
+            >
+              DOCUMENTATION
+            </li>
+          </Link>
+          <Link href="/blog">
+            <li
+              className={cn("font-mono list-none hover:text-terminal-magenta", {
+                "text-terminal-magenta": ("/blog" in pathname),
+                "text-white": !("/blog" in pathname),
+              })}
+            >
+              SOURCE
+            </li>
+          </Link>
+        </div>
+      </nav>
+      <div className="grow h-full w-full bg-deep-slate relative">
+        {children}
+      </div>
+      <footer
+        className="bg-gray-50 p-8 pb-16 w-full font-mono z-40 absolute"
+        ref={refFooter}
+      >
+        <div className="w-full border-t-4 border-double border-deep-slate pt-8">
+          <div className="flex flex-col w-full text-deep-slate items-start space-y-1 text-xl lg:text-4xl font-medium tracking-tighter cursor-default select-none shrink-0 mb-8 lg:mb-0">
+            <p className="font-sans font-medium">
+              Nolita
+            </p>
+            <p className="text-sm tracking-tight">A product of <Link className="border-b border-black hover:border-terminal-magenta" href="https://hdr.is">High Dimensional Research</Link>.</p>
+          </div>
+          <div className="pt-8 flex flex-col text-3xl font-sans tracking-tight font-light">
+            <Link href="/start">
+              Get Started
+            </Link>
+            <Link href="/start">
+              Documentation
+            </Link>
+            <Link href="/start">
+              Source
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </Fragment>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,35 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Iosevka Aile", "sans-serif"],
+      mono: ["Berkeley Mono", "monospace"],
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--gradient-color-stops))",
+        "fade-to-black":
+          "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%)",
+      },
+      colors: {
+        "deep-slate": "#1a1d1f",
+        // 16 color terminal
+        "terminal-black": "#000000",
+        "terminal-white": "#ffffff",
+        "terminal-red": "#ff0000",
+        "terminal-green": "#00751b",
+        "terminal-lime": "#00ff00",
+        "terminal-olive": "#77741d",
+        "terminal-yellow": "#ffff00",
+        "terminal-blue": "#0000ff",
+        "terminal-navy": "#000771",
+        "terminal-purple": "#750071",
+        "terminal-magenta": "#ff00ff",
+        "terminal-cyan": "#00ffff",
+        "terminal-gray": "#757575",
+        "terminal-silver": "#b8b8b8",
+        "terminal-teal": "#007674",
+        "terminal-maroon": "#780003",
       },
     },
   },
