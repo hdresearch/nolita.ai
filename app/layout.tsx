b034e210f212a32bd077e4c7078810f9e89eacae
Matilde Park <matilde@park.net>
2024-04-18 15:33:28 -0400

    layout: add card

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,15 @@ import NavWrapper from "./components/Nav";
 export const metadata: Metadata = {
   title: "Nolita, by High Dimensional Research",
   description: "Your next full-stack framework for agentic applications.",
+  openGraph: {
+    images: "https://content.hdr.is/nolita-card.png",
+    title: "Nolita, by High Dimensional Research",
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: "https://content.hdr.is/nolita-card.png",
+    site: "@highdimrsch"
+  }
 };
 
 export default function RootLayout({
