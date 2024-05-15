8da50f83bf410e9f9939218784ed194cd9b4b514
Matilde Park <matilde.park@gmail.com>
2024-05-15 14:28:51 -0400

    style: add markdown styling

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,7 +1,16 @@
 export default function DocsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 return (
-    <article className="min-h-screen py-24 px-8">
+    <section className="min-h-screen relative py-24 px-8 flex gap-8 justify-center">
+        <nav className="fixed top-24 left-8 min-h-screen">
+            <ul className="text-3xl tracking-tight font-light">
+                <li><a href="/docs">Get Started</a></li>
+                <li><a href="/docs/faq">FAQ</a></li>
+                <li><a href="/docs/api">API Reference</a></li>
+            </ul>
+        </nav>
+    <article className="markdown">
         {children}
     </article>
+    </section>
 )
 }
