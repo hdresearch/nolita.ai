43055fb3e0d5e48e3714ec46953cd6d4db31703c
Matilde Park <matilde.park@gmail.com>
2024-05-16 16:42:13 -0400

    content: move getting started to a folder

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         return [
             {
                 source: '/start',
-                destination: '/docs/start',
+                destination: '/docs/get-started',
                 permanent: true,
             },
         ];
