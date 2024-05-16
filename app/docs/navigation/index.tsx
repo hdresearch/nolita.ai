5b6a8ea64e07a5d8f44e7ba9763284b4c7b65c27
Matilde Park <matilde.park@gmail.com>
2024-05-16 16:41:33 -0400

    docs: nav should auto-open as we nav

--- app/docs/navigation/index.tsx
+++ app/docs/navigation/index.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { Page } from "@/lib/fetch";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from 'next/navigation';
+
 export default function DocsNavigation({ files }: Readonly<{ files: Page[] }>) {
     return (
         <nav className="xl:fixed top-24 left-8 xl:min-h-screen border lg:border-0 border-terminal-magenta">
             <div className="font-mono uppercase p-1 w-full bg-terminal-magenta text-black lg:hidden">NAV</div>
-            <ul className="p-2 max-h-[250px] overflow-y-auto lg:max-h-[auto]">
+            <ul className="p-2 lg:px-0 max-h-[500px] overflow-y-auto lg:max-h-full">
                 {files.map((file) => {
                     if (file.children.length > 0) {
                         return <NavDirectory key={file.path} file={file} />;
@@ -20,6 +22,14 @@ export default function DocsNavigation({ files }: Readonly<{ files: Page[] }>) {
 
 function NavDirectory ({ file, className = "" }: Readonly<{ file: Page, className?: string }>) {
     const [isOpen, setIsOpen] = useState(false);
+    const pathName = usePathname();
+
+    useEffect(() => {
+        if (pathName.startsWith(`/docs${file.path}`)) {
+            setIsOpen(true);
+        }
+    }, [file.path, pathName]);
+
     return (
         <li className={className}>
             <button className="text-lg lg:text-3xl tracking-tight font-light text-left hover:text-terminal-gray" onClick={() => setIsOpen(!isOpen)}>{file.title}</button>
@@ -40,7 +50,7 @@ function NavDirectory ({ file, className = "" }: Readonly<{ file: Page, classNam
 
 function NavPage ({ file, className = "" }: Readonly<{ file: Page, className?: string }>) {
     return (
-        <li className={className + " hover:text-terminal-magenta cursor-pointer text-xs lg:text-base"}>
+        <li className={className + " hover:text-terminal-magenta cursor-pointer text-xs lg:text-xl"}>
             <a href={`/docs${file.path}`}>{file.title}</a>
         </li>
     );
