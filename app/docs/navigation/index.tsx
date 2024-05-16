8805645f04a70618a5426db7aadf2bc144fc02b1
Matilde Park <matilde.park@gmail.com>
2024-05-16 16:18:27 -0400

    layout: mobile

--- app/docs/navigation/index.tsx
+++ app/docs/navigation/index.tsx
@@ -3,8 +3,9 @@ import { Page } from "@/lib/fetch";
 import { useState } from "react";
 export default function DocsNavigation({ files }: Readonly<{ files: Page[] }>) {
     return (
-        <nav className="xl:fixed top-24 left-8 xl:min-h-screen">
-            <ul>
+        <nav className="xl:fixed top-24 left-8 xl:min-h-screen border lg:border-0 border-terminal-magenta">
+            <div className="font-mono uppercase p-1 w-full bg-terminal-magenta text-black lg:hidden">NAV</div>
+            <ul className="p-2 max-h-[250px] overflow-y-auto lg:max-h-[auto]">
                 {files.map((file) => {
                     if (file.children.length > 0) {
                         return <NavDirectory key={file.path} file={file} />;
@@ -21,7 +22,7 @@ function NavDirectory ({ file, className = "" }: Readonly<{ file: Page, classNam
     const [isOpen, setIsOpen] = useState(false);
     return (
         <li className={className}>
-            <button className="text-3xl tracking-tight font-light text-left hover:text-terminal-gray" onClick={() => setIsOpen(!isOpen)}>{file.title}</button>
+            <button className="text-lg lg:text-3xl tracking-tight font-light text-left hover:text-terminal-gray" onClick={() => setIsOpen(!isOpen)}>{file.title}</button>
             {isOpen && (
                 <ul>
                     {file.children.map((child) => {
@@ -39,7 +40,7 @@ function NavDirectory ({ file, className = "" }: Readonly<{ file: Page, classNam
 
 function NavPage ({ file, className = "" }: Readonly<{ file: Page, className?: string }>) {
     return (
-        <li className={className + " hover:text-terminal-magenta cursor-pointer"}>
+        <li className={className + " hover:text-terminal-magenta cursor-pointer text-xs lg:text-base"}>
             <a href={`/docs${file.path}`}>{file.title}</a>
         </li>
     );
