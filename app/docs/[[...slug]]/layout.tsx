dbb69f76ef14685ef9ee0dc33f5a2e7e192bceec
Matilde Park <matilde.park@gmail.com>
2024-05-16 16:41:05 -0400

    layout: mobile tweak

--- app/docs/[[...slug]]/layout.tsx
+++ app/docs/[[...slug]]/layout.tsx
@@ -6,7 +6,7 @@ export default function DocsLayout({ children }: Readonly<{ children: React.Reac
     const docsDirectory = path.join(process.cwd(), 'content/docs');
     const files = buildPageTree(docsDirectory);
 return (
-    <section className="min-h-screen relative py-24 px-2 lg:px-8 flex flex-col lg:flex-row gap-8 lg:justify-center">
+    <section className="min-h-screen relative py-24 px-2 lg:px-8 flex flex-col lg:flex-row gap-8 xl:justify-center">
         <DocsNavigation files={files}/>
     <article className="markdown">
         {children}
